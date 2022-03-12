c1a010a80e10325cce11ee9866ba1585eacc9550
EWSoftware <Eric@EWoodruff.us>
2022-03-12 13:06:46 -0800

    Initial commit
    Moved the HTML to MAML conversion tool out of SHFB into its own project.

---
+++ README.md
@@ -0,0 +1,11 @@
+Welcome to the **HTML to MAML Converter** project.
+
+This tool is used to convert legacy HTML topic files to MAML topics along with their supporting project files so
+that they can be used with current releases of the [Sandcastle Help File Builder](https://github.com/EWSoftware/SHFB)
+project.
+
+This project used to be part of the help file builder project.  However, given that it has been many years since
+the help file builder transitioned to MAML topics for its conceptual content, this tool has been moved into its
+own standalone project and is no longer distributed with the help file builder.
+
+For more information, see the [HTML to MAML Converter documentation](https://ewsoftware.github.io/HtmlToMamlDocs).
---
+++ Source/GlobalSuppressions.cs
@@ -0,0 +1,26 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "<Pending>", Scope = "member", Target = "~M:HtmlToMamlConversion.ConvertHtmlToMaml.Main(System.String[])~System.Int32")]
+[assembly: SuppressMessage("Maintainability", "CA1508:Avoid dead conditional code", Justification = "<Pending>", Scope = "member", Target = "~M:HtmlToMamlConversion.HtmlToMaml.OnMatchImage(System.Text.RegularExpressions.Match)~System.String")]
+[assembly: SuppressMessage("Maintainability", "CA1508:Avoid dead conditional code", Justification = "<Pending>", Scope = "member", Target = "~M:HtmlToMamlConversion.HtmlToMaml.OnMatchAnchor(System.Text.RegularExpressions.Match)~System.String")]
+[assembly: SuppressMessage("Globalization", "CA1308:Normalize strings to uppercase", Justification = "<Pending>", Scope = "member", Target = "~M:HtmlToMamlConversion.HtmlToMaml.OnMatchReplace(System.Text.RegularExpressions.Match)~System.String")]
+[assembly: SuppressMessage("Globalization", "CA1308:Normalize strings to uppercase", Justification = "<Pending>", Scope = "member", Target = "~M:HtmlToMamlConversion.HtmlToMaml.#ctor(System.String,System.String,System.Boolean,System.Boolean)")]
+[assembly: SuppressMessage("Globalization", "CA1308:Normalize strings to uppercase", Justification = "<Pending>", Scope = "member", Target = "~M:HtmlToMamlConversion.HtmlToMaml.ConvertTopic(HtmlToMamlConversion.Topic)")]
+[assembly: SuppressMessage("Design", "CA1062:Validate arguments of public methods", Justification = "<Pending>", Scope = "member", Target = "~M:HtmlToMamlConversion.HtmlToMaml.ConvertTopic(HtmlToMamlConversion.Topic)")]
+[assembly: SuppressMessage("Design", "CA1062:Validate arguments of public methods", Justification = "<Pending>", Scope = "member", Target = "~M:HtmlToMamlConversion.HtmlToMaml.CreateCompanionFile(System.String,HtmlToMamlConversion.Topic)")]
+[assembly: SuppressMessage("Maintainability", "CA1508:Avoid dead conditional code", Justification = "<Pending>", Scope = "member", Target = "~M:HtmlToMamlConversion.HtmlToMaml.CreateCompanionFile(System.String,HtmlToMamlConversion.Topic)")]
+[assembly: SuppressMessage("Security", "CA3075:Insecure DTD processing in XML", Justification = "<Pending>", Scope = "member", Target = "~M:HtmlToMamlConversion.HtmlToMaml.#ctor(System.String,System.String,System.Boolean,System.Boolean)")]
+[assembly: SuppressMessage("Security", "CA5372:Use XmlReader For XPathDocument", Justification = "<Pending>", Scope = "member", Target = "~M:HtmlToMamlConversion.HtmlToMaml.#ctor(System.String,System.String,System.Boolean,System.Boolean)")]
+[assembly: SuppressMessage("Maintainability", "CA1508:Avoid dead conditional code", Justification = "<Pending>", Scope = "member", Target = "~M:HtmlToMamlConversion.ImageReferenceCollection.Save(System.String)")]
+[assembly: SuppressMessage("Design", "CA1062:Validate arguments of public methods", Justification = "<Pending>", Scope = "member", Target = "~M:HtmlToMamlConversion.TagOptions.#ctor(System.Xml.XPath.XPathNavigator)")]
+[assembly: SuppressMessage("Design", "CA1062:Validate arguments of public methods", Justification = "<Pending>", Scope = "member", Target = "~M:HtmlToMamlConversion.TagOptions.Evaluate(System.Text.RegularExpressions.Match)")]
+[assembly: SuppressMessage("Maintainability", "CA1508:Avoid dead conditional code", Justification = "<Pending>", Scope = "member", Target = "~M:HtmlToMamlConversion.TopicCollection.Save(System.String)")]
+[assembly: SuppressMessage("Design", "CA1062:Validate arguments of public methods", Justification = "<Pending>", Scope = "member", Target = "~M:HtmlToMamlConversion.ConvertHtmlToMaml.Main(System.String[])~System.Int32")]
+[assembly: SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "<Pending>", Scope = "member", Target = "~P:HtmlToMamlConversion.FilePath.Exists")]
+[assembly: SuppressMessage("Globalization", "CA1308:Normalize strings to uppercase", Justification = "<Pending>", Scope = "member", Target = "~M:HtmlToMamlConversion.FilePath.GetHashCode~System.Int32")]
+[assembly: SuppressMessage("Design", "CA1036:Override methods on comparable types", Justification = "<Pending>", Scope = "type", Target = "~T:HtmlToMamlConversion.MSHelpKeyword")]
