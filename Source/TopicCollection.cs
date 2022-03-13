d4bbb2df51b15f91a63b73f0847ea857b5b73a03
EWSoftware <Eric@EWoodruff.us>
2022-03-12 18:52:24 -0800

    Updated project website references

--- Source/TopicCollection.cs
+++ Source/TopicCollection.cs
@@ -8,7 +8,7 @@
 // This file contains a class used to contain a collection of topics.
 //
 // This code is published under the Microsoft Public License (Ms-PL).  A copy of the license should be
-// distributed with the code and can be found at the project website: https://GitHub.com/EWSoftware/SHFB.  This
+// distributed with the code and can be found at the project website: https://GitHub.com/EWSoftware/HtmlToMamlConverter.  This
 // notice, the author's name, and all copyright notices must remain intact in all applications, documentation,
 // and source files.
 //
